ff3caa530433b2de1e82246a114d4b0105123b75
J-Hornbeck <90428999+J-Hornbeck@users.noreply.github.com>
2024-11-16 23:17:38 -0600

    Add files via upload

--- public/Section_17/Section 17 Boss Level 1 - The Dicee Game/index.js
+++ public/Section_17/Section 17 Boss Level 1 - The Dicee Game/index.js
@@ -15,9 +15,9 @@ document.querySelector(".img1").setAttribute("src", dice[randomNumber1]);
 document.querySelector(".img2").setAttribute("src", dice[randomNumber2]);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 } else {
-  document.querySelector("h1").innerHTML = "DRAW!";
+  document.querySelector("h1").innerHTML = "🏳️DRAW!🏳️";
 }
